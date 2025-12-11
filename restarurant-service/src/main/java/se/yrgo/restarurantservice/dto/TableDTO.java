40763a675195f941102a1d839747f53d3430b993
GurjotKaur1501 <jotkau0115@gmail.com>
2025-12-11 11:32:07 +0100

    Updating dto classes

--- restarurant-service/src/main/java/se/yrgo/restarurantservice/dto/TableDTO.java
+++ restarurant-service/src/main/java/se/yrgo/restarurantservice/dto/TableDTO.java
@@ -1,4 +1,11 @@
 package se.yrgo.restarurantservice.dto;
 
+import lombok.Data;
+
+@Data
 public class TableDTO {
+    private Long id;
+    private Integer tableNumber;
+    private Integer seats;
+    private Boolean available;
 }
--- restarurant-service/src/main/java/se/yrgo/restarurantservice/dto/TableRequestDTO.java
+++ restarurant-service/src/main/java/se/yrgo/restarurantservice/dto/TableRequestDTO.java
@@ -1,4 +1,20 @@
 package se.yrgo.restarurantservice.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class TableRequestDTO {
+    @NotNull
+    private Integer tableNumber;
+
+    @NotNull
+    @Min(1)
+    private Integer seats;
+
+    private Boolean available = true;
+
+    @NotNull
+    private Long restaurantId;
 }
