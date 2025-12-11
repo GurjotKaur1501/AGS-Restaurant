88861a824874fbe8dfd8113b1d9db902993fffbe
GurjotKaur1501 <jotkau0115@gmail.com>
2025-12-11 12:06:37 +0100

    Adding exception classes

--- restarurant-service/src/main/java/se/yrgo/restarurantservice/exception/ErrorResponse.java
+++ restarurant-service/src/main/java/se/yrgo/restarurantservice/exception/ErrorResponse.java
@@ -1,4 +1,14 @@
 package se.yrgo.restarurantservice.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
 }
--- restarurant-service/src/main/java/se/yrgo/restarurantservice/exception/GlobalExceptionHandler.java
+++ restarurant-service/src/main/java/se/yrgo/restarurantservice/exception/GlobalExceptionHandler.java
@@ -1,4 +1,45 @@
 package se.yrgo.restarurantservice.exception;
 
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public final ResponseEntity<ErrorResponse> handleNotFound(IllegalArgumentException ex, WebRequest request) {
+        ErrorResponse er = new ErrorResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(er, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public final ResponseEntity<ErrorResponse> handleConstraint(ConstraintViolationException ex, WebRequest request) {
+        ErrorResponse er = new ErrorResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(er, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorResponse> handleAll(Exception ex, WebRequest request) {
+        ErrorResponse er = new ErrorResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(er, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // To handle @Valid body errors:
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    public final ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex, WebRequest request) {
+        String details = ex.getBindingResult().getFieldErrors().stream()
+                .map(e -> e.getField() + ": " + e.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        ErrorResponse er = new ErrorResponse(LocalDateTime.now(), "Validation failed", details);
+        return new ResponseEntity<>(er, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
 }
