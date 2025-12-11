24df40205cd80689bc8b071b16f51879d8e6b135
GurjotKaur1501 <jotkau0115@gmail.com>
2025-12-11 11:30:49 +0100

    Update RestaurantRequestDTO.java

--- restarurant-service/src/main/java/se/yrgo/restarurantservice/dto/RestaurantRequestDTO.java
+++ restarurant-service/src/main/java/se/yrgo/restarurantservice/dto/RestaurantRequestDTO.java
@@ -1,4 +1,18 @@
 package se.yrgo.restarurantservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class RestaurantRequestDTO {
+    @NotBlank
+    @Size(min = 2)
+    private String name;
+
+    @NotBlank
+    private String address;
+
+    @NotBlank
+    private String cuisineType;
 }
