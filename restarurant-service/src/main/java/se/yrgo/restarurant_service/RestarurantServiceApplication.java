b23f104cc9873a416f0e4742bbf74725e984a511
GurjotKaur1501 <jotkau0115@gmail.com>
2025-12-11 10:57:33 +0100

    Adding restaurant-service

---
+++ restarurant-service/src/main/java/se/yrgo/restarurant_service/RestarurantServiceApplication.java
@@ -0,0 +1,13 @@
+package se.yrgo.restarurant_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestarurantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestarurantServiceApplication.class, args);
+	}
+
+}
---
+++ restarurant-service/src/test/java/se/yrgo/restarurant_service/RestarurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.yrgo.restarurant_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestarurantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
